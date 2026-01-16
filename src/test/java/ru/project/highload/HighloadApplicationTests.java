502cd05d07d50e841b3f9113455607882d6a02b0
anatoliy <anattoliy90@gmail.com>
2026-01-16 19:25:13 +0300

    add spring

---
+++ src/test/java/ru/project/highload/HighloadApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.highload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
